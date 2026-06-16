ecbbd1e3c6a647b8a5c8a18284e5ad2cf942e7f0
Mica Cisneros <cisnerosmica046@gmail.com>
2026-06-16 00:50:19 -0300

    Ejercicio 3 terminado

--- Program.cs
+++ Program.cs
@@ -46,3 +46,79 @@ if (int.TryParse(texto, out numero))
     Console.WriteLine("Error, el dato ingresado no es valido. Usted debe ingresar un numero entero.");
 }
 
+//Ejercicio 3. Ingrese al Branch CalculadoraV2 para implementar las mejoras en la calculadora.. Solicite al usuario un número y muestre por pantalla:
+//● El valor absoluto de un número
+//● El cuadrado
+//● La raíz cuadrada
+//● El seno
+//● El Coseno
+//● La parte entera de un tipo float.
+//Luego de esto, solicite dos números al usuario y determine:
+//● El Máximo entre los dos números
+//● El Mínimo entre los dos números
+//Para TODOS los casos, no olvide contemplar siempre el caso de que el usuario no ingrese un número válido.
+
+//solisitamos al usuario un numero 
+Console.WriteLine("Ingrese un numero: ");
+string texto3 = Console.ReadLine();
+
+float numero3;
+
+//validamos que sea un numero
+if (float.TryParse (texto3, out numero3))
+{
+    //mostraremos los resultados utilizando la clase Math
+    //valor absoluto
+    Console.WriteLine("Valor absoluto: " + Math.Abs(numero3));
+
+    //el cuadrado
+    Console.WriteLine("Cuadrado: " + Math.Pow(numero3, 2));//(base, exponente)
+
+    //raiz cuadrada, tenemos en cuenta que la raiz existe solamente para numero mayores o iguales a 0
+    if (numero3 >= 0)
+    {
+        Console.WriteLine("Raiz cuadrada: " + Math.Sqrt(numero3));
+    } else
+    {
+        Console.WriteLine("No existe raiz cuadrada para numeros negativos");
+    }
+
+    //seno
+    Console.WriteLine("Seno: " + Math.Sin(numero3));
+
+    //coseno
+    Console.WriteLine("Coseno: " + Math.Cos(numero3));
+
+    //parte entera
+    Console.WriteLine("Parte entera: " + Math.Truncate(numero3));
+    
+}else
+{
+    Console.WriteLine("Error. Debe ingresar un numero valido.");
+}
+
+//solisitamos al usuario que ingrese dos numeros
+Console.WriteLine("Ingrese el primer numero: ");
+string texto4 = Console.ReadLine();
+Console.WriteLine("Ingrese el segundo numero: ");
+string texto5 = Console.ReadLine();
+
+float num1, num2;
+
+//validamos ambos numeros
+if (float.TryParse(texto4, out num1) && float.TryParse(texto4, out num2))
+{
+    //determinamos maximo y minimo
+    if (num1 > num2)
+    {
+        Console.WriteLine("Maximo: " + num1);
+        Console.WriteLine("Minimo: " + num2);
+    } else
+    {
+        Console.WriteLine("Maximo: " + num2);
+        Console.WriteLine("Minimo: " + num1);
+    }
+} else
+{
+    Console.WriteLine("Error. Debe ingresar numeros validos");
+}
